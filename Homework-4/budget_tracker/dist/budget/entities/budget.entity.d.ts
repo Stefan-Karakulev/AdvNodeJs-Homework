0a59be4d5a43f5682367f3a44b946573b2c63d24
Stefan Karakulev <stefankarakulev1@gmail.com>
2024-04-30 12:49:23 +0200

    Pull

--- Homework-4/budget_tracker/dist/budget/entities/budget.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Expense } from 'src/iterface';
-import { Income } from 'src/iterface';
-export declare class BudgetEntity {
-    id: string;
-    title: string;
-    currency: string;
-    expenses: Expense[];
-    incomes: Income[];
-}
